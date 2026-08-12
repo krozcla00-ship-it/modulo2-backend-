e9d60d559cc0436fd11c9d8b7e221da282b2068a
EDGAR <krozcla00@gmail.com>
2026-08-11 20:11:32 -0500

    archivos repetidos eliminados

--- src/models/users.model.js
+++
@@ -1,9 +0,0 @@
-import { text } from "express";
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    email: {type: String, required: true},
-    text: {type: String, required: true},
-
-});

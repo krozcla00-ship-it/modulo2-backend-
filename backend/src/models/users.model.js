882141a576418fdd2b88f7f6d46fd9a789ab10cd
EDGAR <krozcla00@gmail.com>
2026-08-11 20:47:05 -0500

    arreglo

---
+++ backend/src/models/users.model.js
@@ -0,0 +1,14 @@
+import { text } from "express";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+    name: {type: String, required: true},
+
+    email: {type: String, required: true},
+
+    text: {type: String, required: true},
+
+});
+
+export const userModel = mongoose.model('users', userSchema);
